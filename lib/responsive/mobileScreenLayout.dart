00963cd4c384915591ab63dc4c9cc00f19cfc675
roshan-lab <roshq.1234@gmail.com>
2023-07-27 01:15:04 +0530

    Bottom navigation bar added to the app.

--- lib/responsive/mobileScreenLayout.dart
+++ lib/responsive/mobileScreenLayout.dart
@@ -1,8 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram/model/userModel.dart';
 import 'package:instagram/provider/userProvider.dart';
+import 'package:instagram/utils/colors.dart';
 import 'package:provider/provider.dart';
 import 'package:instagram/model/userModel.dart' as model;
 // String username = "";
@@ -15,14 +17,26 @@ class MobileScreenLayout extends StatefulWidget {
 }
 
 class _MobileScreenLayoutState extends State<MobileScreenLayout> {
+  int page=0;
+  late PageController pageController;
+  navigationTapped(int page){
+    pageController.jumpToPage(page);
+  }
   @override
   void initState() {
 
-
+    pageController=PageController();
     /////  this is just to print the user name from data base
     //getusername();
     // TODO: implement initState
     super.initState();
+  }
+
+  @override
+  void dispose() {
+    pageController.dispose();
+    // TODO: implement dispose
+    super.dispose();
   }
 
   ///// function to print user name from data base s
@@ -37,13 +51,43 @@ class _MobileScreenLayoutState extends State<MobileScreenLayout> {
   //   });
   // }
 
+  onPageChange(int _page){
+    setState(() {
+      page=_page;
+    });
+  }
+
   Widget build(BuildContext context) {
-    model.User user=Provider.of<UserProvider>(context).getUser;
+
+    //// by this we can show any data of user to screen
+    //model.User user=Provider.of<UserProvider>(context).getUser;
     return Scaffold(
-      body: Center(
-          child: Text(user.email,
-        style: TextStyle(color: Colors.white),
-      )),
+      body: PageView(
+        controller: pageController,
+        onPageChanged: onPageChange,
+        children: [
+          Text("feed"),
+          Text("fe"),
+          Text("ed"),
+          Text("eed"),
+          Text("f"),
+        ],
+      ),
+      bottomNavigationBar: CupertinoTabBar(
+        backgroundColor: mobileBackgroundColor,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home,color: page==0?primaryColor:secondaryColor,),label: "",backgroundColor: primaryColor),
+          BottomNavigationBarItem(icon: Icon(Icons.search,color: page==1?primaryColor:secondaryColor),label: "",backgroundColor: primaryColor),
+          BottomNavigationBarItem(icon: Icon(Icons.add_circle,color: page==2?primaryColor:secondaryColor),label: "",backgroundColor: primaryColor),
+          BottomNavigationBarItem(icon: Icon(Icons.favorite,color: page==3?primaryColor:secondaryColor),label: "",backgroundColor: primaryColor),
+          BottomNavigationBarItem(icon: Icon(Icons.person,color: page==4?primaryColor:secondaryColor),label: "",backgroundColor: primaryColor),
+        ],
+        onTap: navigationTapped,
+      ),
+      // body: Center(
+      //     child: Text("mobile",
+      //   style: TextStyle(color: Colors.white),
+      // )),
     );
   }
 }
